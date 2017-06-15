108934e1e582014d5e4149d28b91d370e0b5d67a
Arjun Gupta <saimzn4241@gmail.com>
2017-06-15 12:30:17 +0530

    init2

--- heroku/views.py
+++ heroku/views.py
@@ -1,7 +1,10 @@
+from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the heroku index.")
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the heroku index.")
+    return render(request, "index.html", {})
